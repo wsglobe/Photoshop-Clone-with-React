b4fd6d433210af413d1ad5c5fe0f819dd8766329
Walter Wang <wsglobe@gmail.com>
2020-10-15 15:43:31 -0500

    slider

---
+++ photoshop_/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="main-image" />
+      <div className="sidebar">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App;
