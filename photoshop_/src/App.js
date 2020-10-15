5ee288d057e6872b6b221777c8b7bd9ade5ddab8
Walter Wang <wsglobe@gmail.com>
2020-10-15 16:19:01 -0500

    useState hook

--- photoshop_/src/App.js
+++ photoshop_/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Slider from "./Slider"
 import SidebarItem from './SidebarItem'
+
 
 const DEFAULT_OPTIONS = [
   {
