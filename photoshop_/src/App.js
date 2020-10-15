e263b64c0666a1af5f14908a7ba082294dcd5ce5
Walter Wang <wsglobe@gmail.com>
2020-10-15 16:03:05 -0500

    brightness

--- photoshop_/src/App.js
+++ photoshop_/src/App.js
@@ -3,6 +3,18 @@ import './App.css'
 import Slider from "./Slider"
 import SidebarItem from './SidebarItem'
 
+const DEFAULT_OPTIONS = [
+  {
+    name: 'Brightness',
+    property: 'brightness',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+
 function App() {
   return (
     <div className="container">
